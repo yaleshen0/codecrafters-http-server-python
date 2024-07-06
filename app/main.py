cd5a59cb05644e1649299e1c369b0ebfb9bfe125
yaleshen0 <qhshen22@gmail.com>
2024-07-06 15:58:46 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 # Uncomment this to pass the first stage
 import socket
+import re
+
 DEFAULT_HTTP_VERSION = 'HTTP/1.1'
 CRLF = '\r\n'
 def parse_request_line(request_line):
@@ -8,6 +10,14 @@ def parse_request_line(request_line):
     url = request_parts[1]
     protocol = request_parts[2] if len(request_parts) > 2 else DEFAULT_HTTP_VERSION
     return method, url, protocol
+
+def get_after_echo(str):
+    match = re.search(r'/echo/(.*)', str)
+    if match:
+        return match.group(1)
+    else:
+        return ''
+    
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -19,16 +29,30 @@ def main():
     # conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
     with conn:
         while True:
-            request = conn.recv(1024)
+            request = conn.recv(1024).decode('utf-8')
             if not request:
                 break
-            req_lines = request.decode('utf-8').split(CRLF,1)
+            req_lines = request.split(CRLF,1)
             # _, headers = request.decode('utf-8').split('\r\n', 1)
             method, url, protocol=parse_request_line(req_lines[0])
-            if (url!='/'):
-                conn.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n')
-            else:
-                conn.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
+            echo_string = get_after_echo(url) if get_after_echo(url)!='' else ''
+            echo_length = len(echo_string) if echo_string!='' else 0
+            # print(echo_length)
+            status_line = f'{protocol} 200 OK\r\n' if echo_string!='' else f'{protocol} 404 Not Found\r\n'
+            content_type = 'Content-Type: text/plain\r\n'
+            content_length = f'Content-Length: {echo_length}\r\n\r\n'
+            if echo_string=='':
+                if url=='/':
+                    status_line=f'{protocol} 200 OK\r\n'
+                conn.sendall((status_line+content_type+content_length+echo_string).encode())
+            else:  
+                conn.sendall((status_line+content_type+content_length+echo_string).encode())
+            # match url:
+            #     case '/':
+            #         conn.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n')
+            #     case 
+            #     case _:
+            #         conn.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
             
 if __name__ == "__main__":
     main()
